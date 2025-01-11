48e54cb305cfe4e0d34e3db3f56c3bc2bb85b5e2
Thomas Bergmueller <thomas.bergmueller@authenticvision.com>
2025-01-11 15:57:12 +0100

    Export keystore dtos

--- packages/oi-nest/src/index.ts
+++ packages/oi-nest/src/index.ts
@@ -1,3 +1,8 @@
 export * from './oi.module';
 export * from './oi.service';
 export * from './oi.controller';
+
+export { JsonKeystoreDto } from './dto/jsonKeystore.dto';
+export { GenerateKeystoreDto } from './dto/generate-keystore.dto';
+
+
